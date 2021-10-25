4a74e01b0133f7f0f1254c5ed6e4e45abbed3499
96234 <962349367@qq.com>
2021-10-25 23:15:11 +0800

    1

--- src/main/java/com/mikuac/shiro/handler/EventHandler.java
+++ src/main/java/com/mikuac/shiro/handler/EventHandler.java
@@ -14,6 +14,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.lang.annotation.Annotation;
 
 /**
  * 事件处理器
