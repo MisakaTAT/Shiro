c739ef03c7dbad2bc73179b4f1c20991c57d4878
MM <66833771+w9710mm@users.noreply.github.com>
2021-11-03 09:19:12 +0800

    :construction: Add annotation event example #15

---
+++ src/main/java/com/mikuac/shiro/dto/action/annotation/PrivateMessageHandler.java
@@ -0,0 +1,25 @@
+package com.mikuac.shiro.dto.action.annotation;
+
+
+import com.mikuac.shiro.enums.ContainsPictureEnum;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PrivateMessageHandler {
+
+    String regex() default "none";
+
+
+    long[] senderIds() default {};
+
+    long[] excludeSenderIds() default {};
+
+
+    //是否包含图片
+    ContainsPictureEnum isContainsPicture () default ContainsPictureEnum.OFF;
+
+
+}
