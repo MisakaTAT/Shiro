9b4b89d440f075d713f6aa645a12fa0e8fa73ba8
Zero <i@mikuac.com>
2022-01-07 17:25:02 +0800

    :white_check_mark: Added ShiroUtils tests

--- src/test/java/com/mikuac/shiro/Tests.java
+++ src/test/java/com/mikuac/shiro/Tests.java
@@ -1,17 +1,16 @@
 package com.mikuac.shiro;
 
-import com.mikuac.shiro.bean.MsgChainBean;
 import com.mikuac.shiro.common.limit.ActionRateLimiter;
 import com.mikuac.shiro.common.utils.MsgUtils;
 import com.mikuac.shiro.common.utils.ShiroUtils;
 import junit.framework.TestCase;
+import lombok.val;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -21,35 +20,77 @@ public class Tests {
     private ActionRateLimiter actionRateLimiter;
 
     @Test
-    public void testRateLimiter() throws Exception {
-        double a = actionRateLimiter.acquire();
+    public void rateLimiterTest() throws Exception {
+        val a = actionRateLimiter.acquire();
         TestCase.assertEquals(a, 0.00);
-        double b = actionRateLimiter.acquire();
+        val b = actionRateLimiter.acquire();
         if (b <= 0) {
             TestCase.fail("TokenBucket test failed.");
         }
         Thread.sleep(1000);
-        double c = actionRateLimiter.acquire();
+        val c = actionRateLimiter.acquire();
         TestCase.assertEquals(c, 0.00);
     }
 
     @Test
-    public void testStringToMsgChain() {
-        String stringMsg = "[CQ:at,qq=1140667337]测试消息1[CQ:face,id=1]测试消息2[CQ:video,file=https://test.com/1.mp4][CQ:image,file=test.image,url=https://test.com/2.jpg]\n[CQ:image,file=test.image,url=https://test.com/2.jpg]";
-        String arrayMsg = "[MsgChainBean(type=at, data={qq=1140667337}), MsgChainBean(type=text, data={text=测试消息1}), MsgChainBean(type=face, data={id=1}), MsgChainBean(type=text, data={text=测试消息2}), MsgChainBean(type=video, data={file=https://test.com/1.mp4}), MsgChainBean(type=image, data={file=test.image, url=https://test.com/2.jpg}), MsgChainBean(type=text, data={text=\n}), MsgChainBean(type=image, data={file=test.image, url=https://test.com/2.jpg})]";
-        List<MsgChainBean> list = ShiroUtils.stringToMsgChain(stringMsg);
+    public void stringToArrayMsgTest() {
+        val stringMsg = "[CQ:at,qq=1122334455]测试消息1[CQ:face,id=1]测试消息2[CQ:video,file=https://test.com/1.mp4][CQ:image,file=test.image,url=https://test.com/2.jpg]\n[CQ:image,file=test.image,url=https://test.com/2.jpg]";
+        val arrayMsg = "[MsgChainBean(type=at, data={qq=1122334455}), MsgChainBean(type=text, data={text=测试消息1}), MsgChainBean(type=face, data={id=1}), MsgChainBean(type=text, data={text=测试消息2}), MsgChainBean(type=video, data={file=https://test.com/1.mp4}), MsgChainBean(type=image, data={file=test.image, url=https://test.com/2.jpg}), MsgChainBean(type=text, data={text=\n}), MsgChainBean(type=image, data={file=test.image, url=https://test.com/2.jpg})]";
+        val list = ShiroUtils.stringToMsgChain(stringMsg);
         TestCase.assertNotNull(list);
         TestCase.assertEquals(arrayMsg, list.toString());
     }
 
     @Test
-    public void testMsgUtils() {
-        MsgUtils msgUtils = MsgUtils.builder()
-                .at(1140667337L)
+    public void msgUtilsTest() {
+        val msgUtils = MsgUtils.builder()
+                .at(1122334455L)
                 .text("Hello")
                 .img("https://test.com/1.jpg");
-        String buildMsg = "[CQ:at,qq=1140667337]Hello[CQ:image,file=https://test.com/1.jpg]";
+        val buildMsg = "[CQ:at,qq=1122334455]Hello[CQ:image,file=https://test.com/1.jpg]";
         TestCase.assertEquals(buildMsg, msgUtils.build());
+    }
+
+    @Test
+    public void isAtAllTest() {
+        val atAll = "[CQ:at,qq=all]";
+        val atAllArrayMsg = ShiroUtils.stringToMsgChain(atAll);
+        TestCase.assertNotNull(atAllArrayMsg);
+        val isAtAll = ShiroUtils.isAtAll(atAllArrayMsg);
+        TestCase.assertTrue(isAtAll);
+
+        val atUser = "[CQ:at,qq=1122334455]";
+        val atUserArrayMsg = ShiroUtils.stringToMsgChain(atUser);
+        TestCase.assertNotNull(atUserArrayMsg);
+        val notAtAll = ShiroUtils.isAtAll(atUserArrayMsg);
+        TestCase.assertFalse(notAtAll);
+    }
+
+    @Test
+    public void getAtListTest() {
+        val stringMsg = "[CQ:at,qq=11111][CQ:at,qq=22222][CQ:at,qq=33333]";
+        val arrayMsg = ShiroUtils.stringToMsgChain(stringMsg);
+        TestCase.assertNotNull(arrayMsg);
+        val atList = ShiroUtils.getAtList(arrayMsg);
+        TestCase.assertEquals(3, atList.size());
+    }
+
+    @Test
+    public void getMsgImgUrlListTest() {
+        val stringMsg = "[CQ:image,file=https://test.com/2.jpg][CQ:image,file=https://test.com/2.jpg]";
+        val arrayMsg = ShiroUtils.stringToMsgChain(stringMsg);
+        TestCase.assertNotNull(arrayMsg);
+        val imgUrlList = ShiroUtils.getMsgImgUrlList(arrayMsg);
+        TestCase.assertEquals(2, imgUrlList.size());
+    }
+
+    @Test
+    public void getMsgVideoUrlListTest() {
+        val stringMsg = "[CQ:video,file=https://test.com/1.mp4][CQ:video,file=https://test.com/2.mp4]";
+        val arrayMsg = ShiroUtils.stringToMsgChain(stringMsg);
+        TestCase.assertNotNull(arrayMsg);
+        val videoUrlList = ShiroUtils.getMsgVideoUrlList(arrayMsg);
+        TestCase.assertEquals(2, videoUrlList.size());
     }
 
 }
