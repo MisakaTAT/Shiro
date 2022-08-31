2658119d5956238c5d81e061ceb1340871352fd4
MisakaTAT <i@mikuac.com>
2022-08-31 16:34:30 +0800

    :bug: Fixed resp type error for stranger info

--- src/main/java/com/mikuac/shiro/dto/action/response/StrangerInfoResp.java
+++ src/main/java/com/mikuac/shiro/dto/action/response/StrangerInfoResp.java
@@ -11,19 +11,46 @@ import lombok.Data;
 @Data
 public class StrangerInfoResp {
 
+    /**
+     * QQ 号
+     */
     @JSONField(name = "user_id")
     private long userId;
 
+    /**
+     * 昵称
+     */
     @JSONField(name = "nickname")
     private String nickname;
 
+    /**
+     * 性别 male 或 female 或 unknown
+     */
     @JSONField(name = "sex")
     private String sex;
 
+    /**
+     * 年龄
+     */
     @JSONField(name = "age")
     private int age;
 
+    /**
+     * qid id 身份卡
+     */
     @JSONField(name = "qid")
-    private int qid;
+    private String qid;
+
+    /**
+     * 等级
+     */
+    @JSONField(name = "level")
+    private int level;
+
+    /**
+     * 在线天数？我猜的（
+     */
+    @JSONField(name = "login_days")
+    private int loginDays;
 
 }
