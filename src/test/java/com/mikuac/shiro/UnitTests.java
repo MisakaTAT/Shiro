292eff91ded64df5a48b52c4b8f8b29bac9b7885
MisakaTAT <i@mikuac.com>
2023-03-28 11:46:18 +0800

    :white_check_mark: Update unit tests :D

--- src/test/java/com/mikuac/shiro/UnitTests.java
+++ src/test/java/com/mikuac/shiro/UnitTests.java
@@ -4,8 +4,10 @@ import com.mikuac.shiro.common.limit.RateLimiter;
 import com.mikuac.shiro.common.utils.*;
 import com.mikuac.shiro.core.BotContainer;
 import com.mikuac.shiro.core.BotFactory;
+import com.mikuac.shiro.enums.AtEnum;
 import com.mikuac.shiro.enums.MsgTypeEnum;
 import com.mikuac.shiro.exception.ShiroException;
+import com.mikuac.shiro.model.ArrayMsg;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.Assertions;
@@ -15,7 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * <p>UnitTests class.</p>
@@ -48,47 +53,19 @@ class UnitTests {
 
     @Test
     void testRawToArrayMsg() {
-        val rawMsg = "[CQ:at,qq=1122334455]测试消息1[CQ:face,id=1]测试消息2[CQ:video,file=https://test.com/1.mp4][CQ:image,file=test.image,url=https://test.com/2.jpg]\n[CQ:image,file=test.image,url=https://test.com/2.jpg]";
-        val arrayMsg = ShiroUtils.rawToArrayMsg(rawMsg);
-        Assertions.assertNotNull(arrayMsg);
-        int[] count = {0};
-        arrayMsg.forEach(InternalUtils.consumerWithIndex((item, index) -> {
-            if (MsgTypeEnum.at == item.getType() && index == 0) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("1122334455", item.getData().get("qq"));
-            }
-            if (MsgTypeEnum.text == item.getType() && index == 1) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("测试消息1", item.getData().get("text"));
-            }
-            if (MsgTypeEnum.face == item.getType() && index == 2) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("1", item.getData().get("id"));
-            }
-            if (MsgTypeEnum.text == item.getType() && index == 3) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("测试消息2", item.getData().get("text"));
-            }
-            if (MsgTypeEnum.video == item.getType() && index == 4) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("https://test.com/1.mp4", item.getData().get("file"));
-            }
-            if (MsgTypeEnum.image == item.getType() && index == 5) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("test.image", item.getData().get("file"));
-                Assertions.assertEquals("https://test.com/2.jpg", item.getData().get("url"));
-            }
-            if (MsgTypeEnum.text == item.getType() && index == 6) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("\n", item.getData().get("text"));
-            }
-            if (MsgTypeEnum.image == item.getType() && index == 7) {
-                count[0] = count[0] + 1;
-                Assertions.assertEquals("test.image", item.getData().get("file"));
-                Assertions.assertEquals("https://test.com/2.jpg", item.getData().get("url"));
-            }
-        }));
-        Assertions.assertEquals(8, count[0]);
+        val msg = "[CQ:at,qq=1122334455]测试消息1[CQ:face,id=1]测试消息2[CQ:video,file=https://test.com/1.mp4][CQ:image,file=test1.image,url=https://test.com/1.jpg]\n[CQ:image,file=test2.image,url=https://test.com/2.jpg]";
+        val expected = Arrays.asList(
+                new ArrayMsg().setType(MsgTypeEnum.at).setData(Map.of("qq", "1122334455")),
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "测试消息1")),
+                new ArrayMsg().setType(MsgTypeEnum.face).setData(Map.of("id", "1")),
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "测试消息2")),
+                new ArrayMsg().setType(MsgTypeEnum.video).setData(Map.of("file", "https://test.com/1.mp4")),
+                new ArrayMsg().setType(MsgTypeEnum.image).setData(Map.of("file", "test1.image", "url", "https://test.com/1.jpg")),
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "\n")),
+                new ArrayMsg().setType(MsgTypeEnum.image).setData(Map.of("file", "test2.image", "url", "https://test.com/2.jpg"))
+        );
+        val actual = ShiroUtils.rawToArrayMsg(msg);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
@@ -199,10 +176,95 @@ class UnitTests {
     @Test
     void testShiroException() {
         try {
+            // 抛出自定义异常
             throw new ShiroException("test");
         } catch (ShiroException e) {
+            // 捕获并比较异常信息是否一致
             Assertions.assertEquals("test", e.getMessage());
         }
+    }
+
+    @Test
+    void testAtCheck() {
+        val selfId = 123456789L;
+        val arrayMsg = new ArrayList<ArrayMsg>();
+        arrayMsg.add(new ArrayMsg().setType(MsgTypeEnum.at).setData(Map.of("qq", "123456789")));
+
+        // 测试 AtEnum.NEED 的情况
+        boolean actual1 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NEED);
+        Assertions.assertFalse(actual1);
+
+        // 测试 AtEnum.NOT_NEED 的情况
+        boolean actual2 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NOT_NEED);
+        Assertions.assertTrue(actual2);
+
+        // 清空 arrayMsg 列表
+        arrayMsg.clear();
+
+        // 测试 AtEnum.NEED 的情况，但 selfId 不在 arrayMsg 列表中
+        boolean actual3 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NEED);
+        Assertions.assertTrue(actual3);
+
+        // 测试 AtEnum.NOT_NEED 的情况，但 selfId 不在 arrayMsg 列表中
+        boolean actual4 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NOT_NEED);
+        Assertions.assertFalse(actual4);
+    }
+
+    @Test
+    void testExtractMsg() {
+        // 定义包含特殊格式字符串消息的变量
+        val msg = "[CQ:at,qq=1122334455]测试消息1";
+
+        // 将 msg 字符串转换成 ArrayMsg 数组
+        val arrayMsg = ShiroUtils.rawToArrayMsg(msg);
+
+        // 定义 AtEnum 为 NEED，期望 At 标识被解析并去除
+        val expected1 = "测试消息1";
+        val actual1 = CommonUtils.extractMsg(msg, arrayMsg, AtEnum.NEED, 1122334455L);
+        Assertions.assertEquals(expected1, actual1);
+
+        // 定义 AtEnum 为 NOT_NEED，期望原始消息内容不变
+        val actual2 = CommonUtils.extractMsg(msg, arrayMsg, AtEnum.NOT_NEED, 1122334455L);
+        Assertions.assertEquals(msg, actual2);
+
+        // 定义 AtEnum 为 NEED，但消息中未包含 At 标识，期望原始消息内容不变
+        val expected2 = "测试消息2";
+        val actual3 = CommonUtils.extractMsg(expected2, arrayMsg, AtEnum.NEED, 1122334455L);
+        Assertions.assertEquals(expected2, actual3);
+    }
+
+    @Test
+    void testParseAt() {
+        // 定义包含 At 标识的 ArrayMsg 数组
+        val arrayMsg1 = Arrays.asList(
+                new ArrayMsg().setType(MsgTypeEnum.at).setData(Map.of("qq", "1122334455")),
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "测试消息"))
+        );
+        // 定义期望解析出的 At 标识对应的 ArrayMsg
+        val expected1 = arrayMsg1.get(0);
+        // 调用 parseAt 函数进行测试
+        val actual1 = CommonUtils.parseAt(arrayMsg1, 1122334455L);
+        // 使用 assertEquals 函数比较期望值和实际值是否相等
+        Assertions.assertEquals(expected1, actual1);
+
+        // 定义不包含 At 标识的 ArrayMsg 数组
+        val arrayMsg2 = Collections.singletonList(
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "测试消息"))
+        );
+        // 调用 parseAt 函数进行测试，期望返回 null
+        val actual2 = CommonUtils.parseAt(arrayMsg2, 1122334455L);
+        // 使用 assertNull 函数比较期望值和实际值是否相等
+        Assertions.assertNull(actual2);
+
+        // 定义包含 At 标识但不是机器人账号的 ArrayMsg 数组
+        val arrayMsg3 = Arrays.asList(
+                new ArrayMsg().setType(MsgTypeEnum.at).setData(Map.of("qq", "123456789")),
+                new ArrayMsg().setType(MsgTypeEnum.text).setData(Map.of("text", "测试消息"))
+        );
+        // 调用 parseAt 函数进行测试，期望返回 null
+        val actual3 = CommonUtils.parseAt(arrayMsg3, 1122334455L);
+        // 使用 assertNull 函数比较期望值和实际值是否相等
+        Assertions.assertNull(actual3);
     }
 
 }
