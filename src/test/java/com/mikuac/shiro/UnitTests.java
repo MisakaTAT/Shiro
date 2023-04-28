c46b624aaa779955dadb300641df8e2ca97af122
MisakaTAT <i@mikuac.com>
2023-04-28 16:41:34 +0800

    :white_check_mark: Update chat context unit tests

--- src/test/java/com/mikuac/shiro/UnitTests.java
+++ src/test/java/com/mikuac/shiro/UnitTests.java
@@ -4,13 +4,13 @@ import com.mikuac.shiro.common.limit.RateLimiter;
 import com.mikuac.shiro.common.utils.*;
 import com.mikuac.shiro.core.BotContainer;
 import com.mikuac.shiro.core.BotFactory;
+import com.mikuac.shiro.core.ChatCTX;
 import com.mikuac.shiro.enums.AtEnum;
 import com.mikuac.shiro.enums.MsgTypeEnum;
 import com.mikuac.shiro.exception.ShiroException;
 import com.mikuac.shiro.model.ArrayMsg;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +21,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * <p>UnitTests class.</p>
@@ -46,9 +50,9 @@ class UnitTests {
     @Test
     void testRateLimiter() {
         val a = rateLimiter.tryAcquire(1);
-        Assertions.assertTrue(a);
+        assertTrue(a);
         val b = rateLimiter.acquire(4);
-        Assertions.assertTrue(b);
+        assertTrue(b);
     }
 
     @Test
@@ -65,68 +69,68 @@ class UnitTests {
                 new ArrayMsg().setType(MsgTypeEnum.image).setData(Map.of("file", "test2.image", "url", "https://test.com/2.jpg"))
         );
         val actual = ShiroUtils.rawToArrayMsg(msg);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMsgUtils() {
         val build = MsgUtils.builder().at(1122334455L).text("Hello").img("https://test.com/1.jpg");
         val msg = "[CQ:at,qq=1122334455]Hello[CQ:image,file=https://test.com/1.jpg]";
-        Assertions.assertEquals(msg, build.build());
+        assertEquals(msg, build.build());
     }
 
     @Test
     void testIsAtAll() {
         val atAll = "[CQ:at,qq=all]";
         val atAllArrayMsg = ShiroUtils.rawToArrayMsg(atAll);
-        Assertions.assertNotNull(atAllArrayMsg);
+        assertNotNull(atAllArrayMsg);
         val isAtAll = ShiroUtils.isAtAll(atAllArrayMsg);
-        Assertions.assertTrue(isAtAll);
+        assertTrue(isAtAll);
 
         val atUser = "[CQ:at,qq=1122334455]";
         val atUserArrayMsg = ShiroUtils.rawToArrayMsg(atUser);
-        Assertions.assertNotNull(atUserArrayMsg);
+        assertNotNull(atUserArrayMsg);
         val notAtAll = ShiroUtils.isAtAll(atUserArrayMsg);
-        Assertions.assertFalse(notAtAll);
+        assertFalse(notAtAll);
     }
 
     @Test
     void testGetAtList() {
         val stringMsg = "[CQ:at,qq=11111][CQ:at,qq=22222][CQ:at,qq=33333]";
         val arrayMsg = ShiroUtils.rawToArrayMsg(stringMsg);
-        Assertions.assertNotNull(arrayMsg);
+        assertNotNull(arrayMsg);
         val atList = ShiroUtils.getAtList(arrayMsg);
-        Assertions.assertEquals(3, atList.size());
+        assertEquals(3, atList.size());
     }
 
     @Test
     void testGetMsgImgUrlList() {
         val stringMsg = "[CQ:image,file=https://test.com/2.jpg][CQ:image,file=https://test.com/2.jpg]";
         val arrayMsg = ShiroUtils.rawToArrayMsg(stringMsg);
-        Assertions.assertNotNull(arrayMsg);
+        assertNotNull(arrayMsg);
         val imgUrlList = ShiroUtils.getMsgImgUrlList(arrayMsg);
-        Assertions.assertEquals(2, imgUrlList.size());
+        assertEquals(2, imgUrlList.size());
     }
 
     @Test
     void testGetMsgVideoUrlList() {
         val stringMsg = "[CQ:video,file=https://test.com/1.mp4][CQ:video,file=https://test.com/2.mp4]";
         val arrayMsg = ShiroUtils.rawToArrayMsg(stringMsg);
-        Assertions.assertNotNull(arrayMsg);
+        assertNotNull(arrayMsg);
         val videoUrlList = ShiroUtils.getMsgVideoUrlList(arrayMsg);
-        Assertions.assertEquals(2, videoUrlList.size());
+        assertEquals(2, videoUrlList.size());
     }
 
     @Test
     void testGetNickname() {
         val nickname = ShiroUtils.getNickname(1140667337L);
-        Assertions.assertNotNull(nickname);
-        Assertions.assertEquals("Zero", nickname);
+        assertNotNull(nickname);
+        assertEquals("Zero", nickname);
     }
 
     @Test
     void testDebugMode() {
-        Assertions.assertTrue(log.isDebugEnabled());
+        assertTrue(log.isDebugEnabled());
     }
 
     @Test
@@ -134,27 +138,27 @@ class UnitTests {
         val media = OneBotMedia.builder().file("https://a.com/1.jpg").cache(false).proxy(false).timeout(10);
         val msg = MsgUtils.builder().img(media).build();
         val stringMsg = "[CQ:image,file=https://a.com/1.jpg,cache=0,proxy=0,timeout=10]";
-        Assertions.assertEquals(stringMsg, msg);
+        assertEquals(stringMsg, msg);
     }
 
     @Test
     void testBotFactory() {
         val selfId = 1140667337L;
         val bot = botFactory.createBot(selfId, null);
-        Assertions.assertNotNull(bot);
-        Assertions.assertEquals(selfId, bot.getSelfId());
+        assertNotNull(bot);
+        assertEquals(selfId, bot.getSelfId());
     }
 
     @Test
     void testScanAnnotation() {
         val annotations = new ScanUtils().scanAnnotation("com.mikuac.shiro.annotation");
-        annotations.forEach(a -> Assertions.assertTrue(a.isAnnotation()));
+        annotations.forEach(a -> assertTrue(a.isAnnotation()));
     }
 
     @Test
     void testConsumerWithIndex() {
         val list = Arrays.asList(1, 2, 3);
-        list.forEach(InternalUtils.consumerWithIndex((item, index) -> Assertions.assertEquals(item, list.get(index))));
+        list.forEach(InternalUtils.consumerWithIndex((item, index) -> assertEquals(item, list.get(index))));
     }
 
     @Test
@@ -162,7 +166,7 @@ class UnitTests {
         val raw = "[CQ:at,qq=1122334455]";
         val arrayMsg = ShiroUtils.rawToArrayMsg(raw);
         val cqCode = ShiroUtils.arrayMsgToCode(arrayMsg.get(0));
-        Assertions.assertEquals(raw, cqCode);
+        assertEquals(raw, cqCode);
     }
 
     @Test
@@ -170,7 +174,7 @@ class UnitTests {
         val selfId = 1140667337L;
         botContainer.robots.put(selfId, botFactory.createBot(selfId, null));
         val bot = botContainer.robots.get(selfId);
-        Assertions.assertEquals(selfId, bot.getSelfId());
+        assertEquals(selfId, bot.getSelfId());
     }
 
     @Test
@@ -180,7 +184,7 @@ class UnitTests {
             throw new ShiroException("test");
         } catch (ShiroException e) {
             // 捕获并比较异常信息是否一致
-            Assertions.assertEquals("test", e.getMessage());
+            assertEquals("test", e.getMessage());
         }
     }
 
@@ -192,22 +196,22 @@ class UnitTests {
 
         // 测试 AtEnum.NEED 的情况
         boolean actual1 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NEED);
-        Assertions.assertFalse(actual1);
+        assertFalse(actual1);
 
         // 测试 AtEnum.NOT_NEED 的情况
         boolean actual2 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NOT_NEED);
-        Assertions.assertTrue(actual2);
+        assertTrue(actual2);
 
         // 清空 arrayMsg 列表
         arrayMsg.clear();
 
         // 测试 AtEnum.NEED 的情况，但 selfId 不在 arrayMsg 列表中
         boolean actual3 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NEED);
-        Assertions.assertTrue(actual3);
+        assertTrue(actual3);
 
         // 测试 AtEnum.NOT_NEED 的情况，但 selfId 不在 arrayMsg 列表中
         boolean actual4 = CommonUtils.atCheck(arrayMsg, selfId, AtEnum.NOT_NEED);
-        Assertions.assertFalse(actual4);
+        assertFalse(actual4);
     }
 
     @Test
@@ -221,16 +225,16 @@ class UnitTests {
         // 定义 AtEnum 为 NEED，期望 @ 标识被解析并去除
         val expected1 = "测试消息1";
         val actual1 = CommonUtils.msgExtract(msg, arrayMsg, AtEnum.NEED, 1122334455L);
-        Assertions.assertEquals(expected1, actual1);
+        assertEquals(expected1, actual1);
 
         // 定义 AtEnum 为 NOT_NEED，期望原始消息内容不变
         val actual2 = CommonUtils.msgExtract(msg, arrayMsg, AtEnum.NOT_NEED, 1122334455L);
-        Assertions.assertEquals(msg, actual2);
+        assertEquals(msg, actual2);
 
         // 定义 AtEnum 为 NEED，但消息中未包含 @ 标识，期望原始消息内容不变
         val expected2 = "测试消息2";
         val actual3 = CommonUtils.msgExtract(expected2, arrayMsg, AtEnum.NEED, 1122334455L);
-        Assertions.assertEquals(expected2, actual3);
+        assertEquals(expected2, actual3);
     }
 
     @Test
@@ -245,7 +249,7 @@ class UnitTests {
         // 调用 atParse 函数进行测试
         val actual1 = CommonUtils.atParse(arrayMsg1, 1122334455L);
         // 使用 assertEquals 函数比较期望值和实际值是否相等
-        Assertions.assertEquals(expected1, actual1);
+        assertEquals(expected1, actual1);
 
         // 定义不包含 @ 标识的 ArrayMsg 数组
         val arrayMsg2 = Collections.singletonList(
@@ -254,7 +258,7 @@ class UnitTests {
         // 调用 atParse 函数进行测试，期望返回 null
         val actual2 = CommonUtils.atParse(arrayMsg2, 1122334455L);
         // 使用 assertNull 函数比较期望值和实际值是否相等
-        Assertions.assertNull(actual2);
+        assertNull(actual2);
 
         // 定义包含 @ 标识但不是机器人账号的 ArrayMsg 数组
         val arrayMsg3 = Arrays.asList(
@@ -264,7 +268,46 @@ class UnitTests {
         // 调用 atParse 函数进行测试，期望返回 null
         val actual3 = CommonUtils.atParse(arrayMsg3, 1122334455L);
         // 使用 assertNull 函数比较期望值和实际值是否相等
-        Assertions.assertNull(actual3);
+        assertNull(actual3);
+    }
+
+
+    @Test
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    void testChatCTX() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(3);
+        ChatCTX ctx = new ChatCTX();
+        ctx.create()
+                .next(latch::countDown)
+                .next(latch::countDown)
+                .next(latch::countDown)
+                .execute();
+        latch.await(5, TimeUnit.SECONDS);
+        assertEquals(0, latch.getCount());
+    }
+
+    @Test
+    @SuppressWarnings({"ResultOfMethodCallIgnored", "squid:S2925"})
+    void testChatCTXWithTimeout() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(4);
+        ChatCTX ctx = new ChatCTX();
+        ctx.create()
+                .next(() -> {
+                    try {
+                        Thread.sleep(4000);
+                        ctx.reset(5);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    latch.countDown();
+                })
+                .next(latch::countDown)
+                .next(latch::countDown)
+                .next(latch::countDown)
+                .timout(5)
+                .execute();
+        latch.await(10, TimeUnit.SECONDS);
+        assertEquals(0, latch.getCount());
     }
 
 }
