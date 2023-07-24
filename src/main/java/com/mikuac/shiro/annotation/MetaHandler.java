3847bb8f8f97a32267ba08896a65c6f6da400cc1
HollisMeynell <365246692@qq.com>
2023-07-24 15:56:08 +0800

    add meta event handler

---
+++ src/main/java/com/mikuac/shiro/annotation/MetaHandler.java
@@ -0,0 +1,16 @@
+package com.mikuac.shiro.annotation;
+
+import com.mikuac.shiro.enums.MetaEventEnum;
+
+import java.lang.annotation.*;
+
+
+/***
+ * 使用@MetaHandler 仅支持参数(Bot bot, HeartbeatMetaEvent event) 或 (Bot bot, LifecycleMetaEvent event)
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MetaHandler {
+    MetaEventEnum type() default MetaEventEnum.HEARTBEAT;
+}
---
+++ src/main/java/com/mikuac/shiro/dto/event/meta/HeartbeatMetaEvent.java
@@ -0,0 +1,72 @@
+package com.mikuac.shiro.dto.event.meta;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@EqualsAndHashCode(callSuper = true)
+public class HeartbeatMetaEvent extends MetaEvent {
+
+    @JSONField(name = "time")
+    private Long interval;
+
+    @JSONField(name = "status")
+    private Status status;
+
+    @Data
+    private static class Status {
+        @JSONField(name = "app_initialized")
+        Boolean appInitialized;
+
+        @JSONField(name = "app_enabled")
+        Boolean appEnabled;
+
+        @JSONField(name = "app_good")
+        Boolean appIsGood;
+
+        @JSONField(name = "plugins_good")
+        Boolean pluginsIsGood;
+
+        @JSONField(name = "online")
+        Boolean online;
+
+        @JSONField(name = "stat")
+        StatusStatistics stat;
+
+    }
+
+    @Data
+    private static class StatusStatistics {
+
+        @JSONField(name = "packet_received")
+        Long packet_received;
+
+        @JSONField(name = "packet_sent")
+        Long packet_sent;
+
+        @JSONField(name = "packet_lost")
+        Long packet_lost;
+
+        @JSONField(name = "message_received")
+        Long message_received;
+
+        @JSONField(name = "message_sent")
+        Long message_sent;
+
+        @JSONField(name = "disconnect_times")
+        Long disconnect_times;
+
+        @JSONField(name = "lost_times")
+        Long lost_times;
+
+        @JSONField(name = "last_message_time")
+        Long last_message_time;
+    }
+}
