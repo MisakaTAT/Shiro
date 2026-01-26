ef7c9a27fefb35eb6e9da65fdbf7175a314b5092
FlanChanOwO <badapple495@outlook.com>
2026-01-26 12:46:47 +0800

    增加了`shiro`对虚拟线程的支持，然后补上了之前没提交的消息测试方法（在测试包下）

--- src/main/java/com/mikuac/shiro/task/ShiroTaskPoolConfig.java
+++ src/main/java/com/mikuac/shiro/task/ShiroTaskPoolConfig.java
@@ -3,10 +3,11 @@ package com.mikuac.shiro.task;
 import com.mikuac.shiro.properties.TaskPoolProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
+import org.springframework.boot.thread.Threading;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -26,13 +27,14 @@ public class ShiroTaskPoolConfig {
     }
 
     /**
-     * 线程池配置
+     * 平台线程池配置
      *
      * @return {@link ThreadPoolTaskExecutor}
      */
     @Bean("shiroTaskExecutor")
+    @ConditionalOnThreading(Threading.PLATFORM)
     @ConditionalOnProperty(value = "shiro.task-pool.enable-task-pool", havingValue = "true", matchIfMissing = true)
-    public ThreadPoolTaskExecutor shiroTaskExecutor() {
+    public ThreadPoolTaskExecutor shiroTaskPlatformExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(taskPoolProperties.getCorePoolSize());
         executor.setMaxPoolSize(taskPoolProperties.getMaxPoolSize());
@@ -44,5 +46,28 @@ public class ShiroTaskPoolConfig {
         executor.initialize();
         return executor;
     }
+
+    /**
+     * 虚拟线程池配置
+     *
+     * @return {@link ThreadPoolTaskExecutor}
+     */
+    @Bean("shiroTaskExecutor")
+    @ConditionalOnThreading(Threading.VIRTUAL)
+    @ConditionalOnProperty(value = "shiro.task-pool.enable-task-pool", havingValue = "true", matchIfMissing = true)
+    public ThreadPoolTaskExecutor shiroTaskVirutualExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadFactory(Thread.ofVirtual().factory());
+        executor.setCorePoolSize(taskPoolProperties.getCorePoolSize());
+        executor.setMaxPoolSize(taskPoolProperties.getMaxPoolSize());
+        executor.setQueueCapacity(taskPoolProperties.getQueueCapacity());
+        executor.setKeepAliveSeconds(taskPoolProperties.getKeepAliveTime());
+        executor.setThreadNamePrefix(taskPoolProperties.getThreadNamePrefix());
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
 
 }
